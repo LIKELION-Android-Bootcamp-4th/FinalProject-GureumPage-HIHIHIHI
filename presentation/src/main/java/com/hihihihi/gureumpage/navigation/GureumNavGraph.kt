507b14ce7fd6a44d630df837bd0b5e5ea80c743c
seeho <siki7878@gmail.com>
2025-08-01 21:34:55 +0900

    feat: GureumNavGraph 생성

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/navigation/GureumNavGraph.kt
@@ -0,0 +1,37 @@
+package com.hihihihi.gureumpage.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.hihihihi.gureumpage.ui.bookdetail.BookDetailScreen
+import com.hihihihi.gureumpage.ui.home.HomeScreen
+import com.hihihihi.gureumpage.ui.library.LibraryScreen
+import com.hihihihi.gureumpage.ui.login.LoginScreen
+import com.hihihihi.gureumpage.ui.mindmap.MindMapScreen
+import com.hihihihi.gureumpage.ui.mypage.MyPageScreen
+import com.hihihihi.gureumpage.ui.onboarding.OnBoardingScreen
+import com.hihihihi.gureumpage.ui.quotes.QuotesScreen
+import com.hihihihi.gureumpage.ui.search.SearchScreen
+import com.hihihihi.gureumpage.ui.statistics.StatisticsScreen
+import com.hihihihi.gureumpage.ui.timer.TimerScreen
+
+@Composable
+fun GureumNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = NavigationRoute.Home.route
+    ){
+        composable(NavigationRoute.Home.route) { HomeScreen() }
+        composable(NavigationRoute.Login.route) { LoginScreen() }
+        composable(NavigationRoute.OnBoarding.route) { OnBoardingScreen() }
+        composable(NavigationRoute.MindMap.route) { MindMapScreen() }
+        composable(NavigationRoute.Quotes.route) { QuotesScreen() }
+        composable(NavigationRoute.Library.route) { LibraryScreen() }
+        composable(NavigationRoute.Search.route) { SearchScreen() }
+        composable(NavigationRoute.Statistics.route) { StatisticsScreen() }
+        composable(NavigationRoute.Timer.route) { TimerScreen() }
+        composable(NavigationRoute.MyPage.route) { MyPageScreen() }
+        composable(NavigationRoute.BookDetail.route) { BookDetailScreen() }
+    }
+}
