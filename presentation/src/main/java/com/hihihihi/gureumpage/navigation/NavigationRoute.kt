e45cf202077d38f6989ea0a06fe51cfbbd3d000a
seeho <siki7878@gmail.com>
2025-08-01 21:34:16 +0900

    feat: 임시 .gitkeep 삭제 후 NavigationRoute 생성

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/navigation/NavigationRoute.kt
@@ -0,0 +1,15 @@
+package com.hihihihi.gureumpage.navigation
+
+sealed class NavigationRoute (val route: String) {
+    object Home: NavigationRoute("home")
+    object Login: NavigationRoute("login")
+    object OnBoarding: NavigationRoute("onboarding")
+    object MindMap: NavigationRoute("mindmap")
+    object Quotes: NavigationRoute("quotes")
+    object Library: NavigationRoute("library")
+    object Search: NavigationRoute("search")
+    object Statistics: NavigationRoute("statistics")
+    object Timer: NavigationRoute("timer")
+    object MyPage: NavigationRoute("mypage")
+    object BookDetail: NavigationRoute("bookdetail")
+}
