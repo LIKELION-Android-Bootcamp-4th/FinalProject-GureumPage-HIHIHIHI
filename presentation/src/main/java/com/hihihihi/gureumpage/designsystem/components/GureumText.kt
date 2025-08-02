6ff019ff5a23fd3b3d3148a22943d11aad7745e0
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-02 20:34:29 +0900

    feat: 공통 텍스트 구현

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/designsystem/components/GureumText.kt
@@ -0,0 +1,58 @@
+package com.hihihihi.gureumpage.designsystem.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hihihihi.gureumpage.designsystem.theme.GureumPageTheme
+import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
+
+/**
+ * 스크린 목차 타이틀, 책 상세 책 제목, 마이페이지 버튼 리스트 등
+ */
+@Composable
+fun TitleText(
+    text: String,
+    maxLine: Int = Int.MAX_VALUE,
+    isUnderline: Boolean = false,
+) {
+    val underlineColor: Color = GureumTheme.colors.primary50
+    val underlineModifier = if (isUnderline) {
+        Modifier.drawBehind {
+            val underlineHeight = 9.dp.toPx()
+            drawRect(
+                color = underlineColor,
+                topLeft = Offset(0f, size.height - underlineHeight),
+                size = androidx.compose.ui.geometry.Size(width = size.width, underlineHeight),
+            )
+        }
+    } else {
+        Modifier
+    }
+
+    Text(
+        text,
+        style = MaterialTheme.typography.titleLarge,
+        maxLines = maxLine,
+        modifier = underlineModifier
+    )
+}
+
+@Preview(name = "DarkMode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "LightMode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+private fun TextPreview() {
+    GureumPageTheme {
+        Column {
+            TitleText("필사한 문장", isUnderline = true)
+            TitleText("필사한 문장")
+        }
+    }
+}
