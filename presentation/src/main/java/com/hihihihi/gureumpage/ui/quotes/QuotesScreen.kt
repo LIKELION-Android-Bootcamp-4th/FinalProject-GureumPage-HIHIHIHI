4f9e8870a09b90abf6d9a508136c0694769bd6cd
Ejay <aabc88@nate.com>
2025-08-02 23:35:34 +0900

    feat: 필사 목록 화면 구성 및 더미데이터 적용

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/QuotesScreen.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/QuotesScreen.kt
@@ -1,15 +1,94 @@
 package com.hihihihi.gureumpage.ui.quotes
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun QuotesScreen() {
+    val quotesList = listOf<Quote>(
+        Quote("title1", "date1", "image1", "content1"),
+        Quote("title2", "date2", "image2", "content2"),
+        Quote("title3", "date3", "image3", "content3"),
+    )
+
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Quotes Screen")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            TopAppBar(
+                title = { Text(text = "Quotes", fontWeight = FontWeight.Bold) }
+            )
+
+            LazyColumn(modifier = Modifier.padding(horizontal = 16.dp)) {
+                items(quotesList.size) { index ->
+                    QuoteItem(item = quotesList[index])
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+        }
     }
 }
+
+@Composable
+fun QuoteItem(item: Quote) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(16.dp)),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(modifier = Modifier.padding(12.dp)) {
+            /*Image(
+                painter = rememberAsyncImagePainter(item.image),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )*/ //이미지 예정
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = item.title, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                Text(text = item.date, fontSize = 12.sp, color = Color.Gray)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(item.content, maxLines = 2, overflow = TextOverflow.Ellipsis)
+            }
+        }
+    }
+}
+
+data class Quote(
+    val title: String,
+    val date: String,
+    val image: String,
+    val content: String
+)
