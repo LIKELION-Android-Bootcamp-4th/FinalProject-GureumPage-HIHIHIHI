1f0c871bb7da0fda85521599581853378291c2e4
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-02 12:47:27 +0900

    refactor: 바뀐 컬러 지정 스타일로 컬러스키마 변경

--- presentation/src/main/java/com/hihihihi/gureumpage/designsystem/theme/Theme.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/designsystem/theme/Theme.kt
@@ -1,43 +1,85 @@
 package com.hihihihi.gureumpage.designsystem.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+
+private val LightGureum = GureumColors.defaultLightColors()
+private val DarkGureum = GureumColors.defaultDarkColors()
+
+private val LightColorScheme = lightColorScheme(
+    primary = LightGureum.primary,
+    onPrimary = LightGureum.white,
+    primaryContainer = LightGureum.primaryDeep,
+    onPrimaryContainer = LightGureum.white,
+    inversePrimary = LightGureum.primaryShallow,
+
+    secondary = LightGureum.gray200,
+    onSecondary = LightGureum.gray0,
+//    secondaryContainer = LightGureum.dividerShallow,
+//    onSecondaryContainer = LightGureum.gray100,
+
+    tertiary = LightGureum.primary50,
+    onTertiary = LightGureum.gray500,
+//    tertiaryContainer = LightGureum.primary10,
+//    onTertiaryContainer = LightGureum.gray500,
+
+    error = LightGureum.systemRed,
+    onError = LightGureum.white,
+    errorContainer = LightGureum.systemRed,
+    onErrorContainer = LightGureum.white,
+
+    surface = LightGureum.card,
+    onSurface = LightGureum.gray800,
+//    surfaceContainerLowest = LightGureum.gray800,
+    surfaceContainerLow = LightGureum.gray200,
+    surfaceContainer = LightGureum.card,
+//    surfaceContainerHigh = LightGureum.gray200,
+    surfaceContainerHighest = LightGureum.card,
+
+    background = LightGureum.background,
+    onBackground = LightGureum.gray800,
+
+    outline = LightGureum.gray200,
+    outlineVariant = LightGureum.background50,
+)
 
 private val DarkColorScheme = darkColorScheme(
-    primary = GureumColor.DarkPrimary,
-    onPrimary = GureumColor.DarkGray100,
-    primaryContainer = GureumColor.DarkPrimaryDeep,
-    onPrimaryContainer = GureumColor.White,
-    inversePrimary = GureumColor.DarkPrimaryShallow,
+    primary = DarkGureum.primary,
+    onPrimary = DarkGureum.white,
+    primaryContainer = DarkGureum.primaryDeep,
+    onPrimaryContainer = DarkGureum.white,
+    inversePrimary = DarkGureum.primaryShallow,
 
-    secondary = GureumColor.DarkGray200,
-    onSecondary = GureumColor.DarkGray0,
+    secondary = DarkGureum.gray200,
+    onSecondary = DarkGureum.gray0,
 
-    tertiary = GureumColor.LightPrimary50,
-    onTertiary = GureumColor.DarkGray500,
+    tertiary = DarkGureum.primary50,
+    onTertiary = DarkGureum.gray500,
 
-    error = GureumColor.SystemRed,
-    onError = GureumColor.White,
-    errorContainer = GureumColor.SystemRed,
-    onErrorContainer = GureumColor.White,
+    error = DarkGureum.systemRed,
+    onError = DarkGureum.white,
+    errorContainer = DarkGureum.systemRed,
+    onErrorContainer = DarkGureum.white,
 
-    surface = GureumColor.DarkCard,
-    onSurface = GureumColor.DarkGray100,
-    surfaceContainerLowest = GureumColor.DarkGray100,
-    surfaceContainerLow = GureumColor.DarkGray300,
-    surfaceContainer = GureumColor.DarkCard,
-    surfaceContainerHigh = GureumColor.DarkGray600,
-    surfaceContainerHighest = GureumColor.DarkGray700,
+    surface = DarkGureum.card,
+    onSurface = DarkGureum.gray800,
+//    surfaceContainerLowest = DarkGureum.gray100,
+    surfaceContainerLow = DarkGureum.gray200,
+    surfaceContainer = DarkGureum.card,
+//    surfaceContainerHigh = DarkGureum.gray600,
+    surfaceContainerHighest = DarkGureum.card,
 
-    background = GureumColor.DarkBackground,
-    onBackground = GureumColor.DarkGray0,
+    background = DarkGureum.background,
+    onBackground = DarkGureum.gray0,
 
-    outline = GureumColor.DarkGray200,
-    outlineVariant = GureumColor.LightBackground50,
+    outline = DarkGureum.gray200,
+    outlineVariant = DarkGureum.background50,
 )
 
 private val LightColorScheme = lightColorScheme(
