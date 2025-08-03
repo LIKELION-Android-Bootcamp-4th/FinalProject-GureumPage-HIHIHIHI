4dfb93f776d2bc9c1936da836d9a30eecefc9cc0
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-03 22:36:12 +0900

    refactor : 이미지를 실제 이미지 url로 테스트, width 조정

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/BookItem.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/BookItem.kt
@@ -1,15 +1,12 @@
 package com.hihihihi.gureumpage.ui.library.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
