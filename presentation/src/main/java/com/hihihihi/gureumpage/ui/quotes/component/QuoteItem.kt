5a429a38f081cd15deaa89d557f701eb8b005785
Ejay <aabc88@nate.com>
2025-08-04 00:23:07 +0900

    feat: 기존 Screen에 있던 QuoteItem 분리

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/component/QuoteItem.kt
@@ -0,0 +1,87 @@
+package com.hihihihi.gureumpage.ui.quotes.component
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
+import com.hihihihi.gureumpage.designsystem.theme.GureumTypography
+import com.hihihihi.gureumpage.ui.quotes.Quote
+
+@Composable
+fun QuoteItem(item: Quote, onItemClick: (Quote) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(16.dp))
+            .clickable(onClick = { onItemClick(item) })
+            .background(GureumTheme.colors.background),
+        elevation = CardDefaults.cardElevation(4.dp)
+
+    ) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            AsyncImage(
+                modifier = Modifier
+                    .size(width = 60.dp, height = 80.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop,
+                model = item.image,
+                contentDescription = "책",
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.Start
+            ) {
+                //책 제목
+                Text(
+                    text = item.title,
+                    style = GureumTypography.bodyLarge,
+                    color = GureumTheme.colors.gray600,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                //날짜
+                Text(
+                    text = item.date,
+                    style = GureumTypography.bodyMedium,
+                    color = GureumTheme.colors.gray300,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                //작성한 필사
+                Text(
+                    text = item.content,
+                    style = GureumTypography.bodyMedium,
+                    color = GureumTheme.colors.gray800,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+            }
+        }
+    }
+}
