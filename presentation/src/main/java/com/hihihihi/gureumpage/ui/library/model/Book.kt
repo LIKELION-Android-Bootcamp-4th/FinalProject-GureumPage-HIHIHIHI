1de6a2cfeefa96ab8b770eb415141fefeac0e501
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-03 17:09:57 +0900

    feat: 서재 책 데이터

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/library/model/Book.kt
@@ -0,0 +1,10 @@
+package com.hihihihi.gureumpage.ui.library.model
+
+data class Book(
+    val id: String,
+    val title: String,
+    val author: String,
+//    val publisher: String?,
+    val imageUrl: String,
+    val isRead: Boolean
+)
