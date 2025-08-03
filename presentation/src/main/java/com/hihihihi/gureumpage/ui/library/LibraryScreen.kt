e378425c736fffd65f9c1515b8eb42a4bb58a36f
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-03 22:39:08 +0900

    refactor : 주석 추가와, 구조변경

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/library/LibraryScreen.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/library/LibraryScreen.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
