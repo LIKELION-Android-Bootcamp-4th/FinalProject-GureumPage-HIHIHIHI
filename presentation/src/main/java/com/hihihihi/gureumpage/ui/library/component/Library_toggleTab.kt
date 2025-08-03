898f5108f24f394dc6f2fba05e0f08fb631ee56b
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-03 17:22:35 +0900

    feat: 서재에 탭 바

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/Library_toggleTab.kt
@@ -0,0 +1,51 @@
+package com.hihihihi.gureumpage.ui.library.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
+
+@Composable
+fun ToggleTab(
+    isBeforeReading: Boolean,
+    onToggle: (Boolean) -> Unit
+) {
+    val selectedColor = Color(0xFFFFD460)
+    val unselectedColor = Color(0xFF3A3A3C)
+    val textColor = GureumTheme.colors.gray800
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Button(
+            onClick = {onToggle(true)},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = if(isBeforeReading) selectedColor else unselectedColor
+            ),
+            modifier = Modifier.weight(1f)
+        ) {
+            Text("읽기 전", color = textColor)
+        }
+
+        Button(
+            onClick = { onToggle(false) },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = if(!isBeforeReading) selectedColor else unselectedColor
+            ),
+            modifier = Modifier.weight(1f)
+        ) {
+            Text("읽은 후", color = textColor)
+        }
+    }
+}
