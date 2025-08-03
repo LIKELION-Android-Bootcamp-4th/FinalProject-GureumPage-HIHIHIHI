6a0c3d7b3507e3d2ac7b90bb29405160fc3b1aa6
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-03 22:38:43 +0900

    refactor : 기존에 button 에서 탭으로 교체

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/Library_toggleTab.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/Library_toggleTab.kt
@@ -1,15 +1,18 @@
 package com.hihihihi.gureumpage.ui.library.component
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
 
@@ -18,34 +21,44 @@ fun ToggleTab(
     isBeforeReading: Boolean,
     onToggle: (Boolean) -> Unit
 ) {
-    val selectedColor = GureumTheme.colors.primary
-    val unselectedColor = GureumTheme.colors.gray400
-    val textColor = GureumTheme.colors.gray800
+    //현재 선택된 탭 인덱스 결정
+    val selectedTabIndex = if (isBeforeReading) 0 else 1
+    val tabs = listOf("일기 전", "읽은 후")
 
-    Row(
+    //탭 ui
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 12.dp),
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(GureumTheme.colors.card)
     ) {
-        Button(
-            onClick = {onToggle(true)},
-            colors = ButtonDefaults.buttonColors(
-                containerColor = if(isBeforeReading) selectedColor else unselectedColor
-            ),
-            modifier = Modifier.weight(1f)
+        TabRow(
+            selectedTabIndex = selectedTabIndex,
+            containerColor = GureumTheme.colors.card,
+            contentColor = GureumTheme.colors.primary,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    modifier = Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                    color = GureumTheme.colors.primary
+                )
+            },
         ) {
-            Text("읽기 전", color = textColor)
-        }
+            tabs.forEachIndexed { index, title ->
+                val selected = selectedTabIndex == index
 
-        Button(
-            onClick = { onToggle(false) },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = if(!isBeforeReading) selectedColor else unselectedColor
-            ),
-            modifier = Modifier.weight(1f)
-        ) {
-            Text("읽은 후", color = textColor)
+                Tab(
+                    selected = selected,
+                    onClick = { onToggle(index == 0) },
+                    selectedContentColor = GureumTheme.colors.primary,
+                    unselectedContentColor = GureumTheme.colors.gray500,
+                    text = {
+                        Text(
+                            text = title,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    }
+                )
+            }
         }
     }
 }
