8767a019b0c9416f2918041369f44f3ee489b333
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-03 17:20:58 +0900

    feat: 서재에 표시되는 책 아이템

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/BookItem.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/BookItem.kt
@@ -1,11 +1,15 @@
 package com.hihihihi.gureumpage.ui.library.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
