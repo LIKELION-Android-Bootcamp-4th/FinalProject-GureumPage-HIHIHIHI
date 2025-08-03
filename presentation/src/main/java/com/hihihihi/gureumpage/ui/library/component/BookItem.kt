c8090511b9470e0e73700b77e2e71169ba9fc399
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-03 22:36:12 +0900

    refactor : 이미지를 실제 이미지 url로 테스트, width 조정

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/BookItem.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/library/component/BookItem.kt
@@ -1,14 +1,12 @@
 package com.hihihihi.gureumpage.ui.library.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,62 +14,77 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
 import com.hihihihi.gureumpage.ui.library.model.Book
 
+
 //한 권의 책정보
 @Composable
-fun BookItem(book: Book) {
+fun BookItem(book: Book, modifier: Modifier = Modifier) {
+    Log.d("BookItem", "BookItem 실행됨: ${book.title}")
+    val context = LocalContext.current
+    val imageRequest = ImageRequest.Builder(context)
+        .data(book.imageUrl)
+        .crossfade(true)
+        .listener(
+            onStart = {
+                Log.d("bookItem", "이미지 로딩 시작 : ${book.imageUrl}")
+            },
+            onSuccess = { _, _ ->
+                Log.d("bookItem", "이미지 로딩 성공 : ${book.imageUrl}")
+            },
+            onError = { _, result ->
+                Log.e("bookItem", "이미지 로딩 실패 : ${book.imageUrl}", result.throwable)
+            }
+        ).build()
+    // 책 카드 ui
     Column(
         modifier = Modifier
-            .padding(8.dp)
-            .width(100.dp)
+            .fillMaxWidth()
+            .padding(12.dp),
+        horizontalAlignment = Alignment.Start
     ) {
         //이미지
-        Box(
+        AsyncImage(
+            model = imageRequest,
+            contentDescription = book.title,
             modifier = Modifier
-                .width(100.dp)
-                .height(160.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .background(GureumTheme.colors.bookBackground),
-            contentAlignment = Alignment.Center
-        ) {
-            Text("이미지", color = GureumTheme.colors.gray600)
-        }
+                .fillMaxWidth()
+                .aspectRatio(2f / 3f)
+                .clip(RoundedCornerShape(8.dp)),
+            contentScale = ContentScale.Crop,
+        )
 
-        Spacer(modifier = Modifier.height(4.dp))
+        Spacer(modifier = Modifier.height(8.dp))
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Text(
-                text = book.title,
-                style = MaterialTheme.typography.bodyMedium,
-                color = GureumTheme.colors.gray800,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = book.author,
-                style = MaterialTheme.typography.bodySmall,
-                color = GureumTheme.colors.gray500,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-//        book.publisher?.let {
-//            Text(
-//                text = it,
-//                style = MaterialTheme.typography.labelSmall,
-//                color = Color.Gray
-//            )
-//        }
-        }
+        //제목
+        Text(
+            text = book.title,
+            style = MaterialTheme.typography.bodyMedium,
+            color = GureumTheme.colors.gray800,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.fillMaxWidth()
+        )
 
+        Spacer(modifier = Modifier.height(8.dp))
 
+        //작가
+        Text(
+            text = book.author,
+            style = MaterialTheme.typography.bodySmall,
+            color = GureumTheme.colors.gray500,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
+
 }
