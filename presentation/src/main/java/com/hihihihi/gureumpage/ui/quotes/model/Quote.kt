48df254dec920a708af5ae59cd714d60411ea90d
Ejay <aabc88@nate.com>
2025-08-04 11:36:00 +0900

    refactor: data 분리

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/model/Quote.kt
@@ -0,0 +1,5 @@
+package com.hihihihi.gureumpage.ui.quotes.model
+
+data class Quote(
+    val title: String, val date: String, val image: String, val content: String, val page: Int, val author: String, val publisher: String
+)
