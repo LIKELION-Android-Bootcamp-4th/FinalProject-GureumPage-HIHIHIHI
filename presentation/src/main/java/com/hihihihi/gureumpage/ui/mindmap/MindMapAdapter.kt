71ef2c02ec1f0d0e3ab61409f72541d29fd16ee2
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-05 20:04:58 +0900

    feat: 기본 기능만 가진 마인드맵 어댑터 구현

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/mindmap/MindMapAdapter.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/mindmap/MindMapAdapter.kt
@@ -1,29 +1,40 @@
 package com.hihihihi.gureumpage.ui.mindmap
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import com.gyso.treeview.TreeViewEditor
 import com.gyso.treeview.adapter.DrawInfo
 import com.gyso.treeview.adapter.TreeViewAdapter
 import com.gyso.treeview.adapter.TreeViewHolder
 import com.gyso.treeview.line.BaseLine
 import com.gyso.treeview.model.NodeModel
+import com.hihihihi.gureumpage.R
 import java.util.UUID
 
 typealias TreeNode = NodeModel<MindMapNodeData>
 
 class MindMapAdapter(
-    private val onNodeClick: (TreeNode) -> Unit     // 노드를 클릭했을 때 행동을 받음
+    private val onNodeClick: (TreeNode) -> Unit
 ) : TreeViewAdapter<MindMapNodeData>() {
-    private var selected: TreeNode? = null          // 현재 선택된 노드
-    private lateinit var editor: TreeViewEditor     // 트리 에디터
+    private lateinit var editor: TreeViewEditor // 마인드 맵 편집 객체
+    private var selected: TreeNode? = null      // 현재 선택된 노드
 
-    override fun onCreateViewHolder(parent: ViewGroup, node: TreeNode): TreeViewHolder<MindMapNodeData> =
-        TreeViewHolder(android.widget.TextView(parent.context), node)
+    fun setEditor(treeViewEditor: TreeViewEditor) {
+        editor = treeViewEditor
+        editor.requestMoveNodeByDragging(false) // 화면 드래그 활성화
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, node: TreeNode) =
+        TreeViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_mindmap_node, parent, false), node
+        )
 
     override fun onBindViewHolder(holder: TreeViewHolder<MindMapNodeData>) {
+        holder.view.findViewById<TextView>(R.id.node_text).text = holder.node.value.content
         holder.view.setOnClickListener {
             selected = holder.node
-            onNodeClick(holder.node)
+            onNodeClick(holder.node) // 이벤트 처리할 노드 전달
         }
     }
 
@@ -32,8 +43,10 @@ class MindMapAdapter(
 
     fun addChildToSelected() {
         selected?.let {
-            val newNode = TreeNode(MindMapNodeData(UUID.randomUUID().toString(), "새 노드"))
-            editor.addChildNodes(it, newNode)
+            editor.addChildNodes(
+                it,
+                TreeNode(MindMapNodeData(UUID.randomUUID().toString(), "새 노드"))
+            )
         }
     }
 
