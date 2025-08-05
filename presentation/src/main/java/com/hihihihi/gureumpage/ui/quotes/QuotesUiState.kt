8909f2b55ac329dec14044d3308b3af5fd5086a3
Ejay <aabc88@nate.com>
2025-08-05 16:12:32 +0900

    refactor: 상태 관리 추가

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/QuotesUiState.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/QuotesUiState.kt
@@ -1,10 +1,10 @@
 package com.hihihihi.gureumpage.ui.quotes
 
-import com.hihihihi.domain.model.UserBook
+import com.hihihihi.domain.model.Quote
 
 // 홈 화면의 UI 상태를 나타내는 데이터 클래스
 data class QuotesUiState(
     val isLoading: Boolean = false, // 데이터 로딩 중인지 여부
-    val books: List<UserBook> = emptyList(), // 사용자 책 목록
+    val quotes: List<Quote> = emptyList(), // 사용자 필사 목록
     val errorMessage: String? = null, // 에러 메세지 (null이면 에러 없음)
 )
--- presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/QuotesViewModel.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/QuotesViewModel.kt
@@ -14,14 +14,35 @@ import javax.inject.Inject
 @HiltViewModel
 class QuotesViewModel @Inject constructor(
     private val getQuoteUseCase: GetQuoteUseCase
-): ViewModel() {
+) : ViewModel() {
     private val _quotes = MutableStateFlow<List<Quote>>(emptyList())
     val quotes: StateFlow<List<Quote>> = _quotes.asStateFlow()
 
-    fun getQuotes(userId: String) {
+    private val _uiState = MutableStateFlow(QuotesUiState(isLoading = true))
+    val uiState: StateFlow<QuotesUiState> = _uiState.asStateFlow()
+
+    init {
+        getQuotes("iK4v1WW1ZX4gID2HueBi")
+    }
+
+    /*fun getQuotes(userId: String) {
         viewModelScope.launch {
             getQuoteUseCase(userId).collect {
                 _quotes.value = it
+            }
+        }
+    }*/
+
+    fun getQuotes(userId: String) {
+        viewModelScope.launch {
+            try {
+                _uiState.value = QuotesUiState(isLoading = true)
+                getQuoteUseCase(userId).collect { quotes ->
+                    _uiState.value = QuotesUiState(quotes = quotes, isLoading = false)
+                }
+            } catch (e: Exception) {
+                _uiState.value =
+                    QuotesUiState(errorMessage = e.message ?: "알 수 없는 오류 발생", isLoading = false)
             }
         }
     }
