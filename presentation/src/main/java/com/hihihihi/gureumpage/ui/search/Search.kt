7cd65ef0131c6c50efbc47233c10ed2b9b9eba5c
Ejay <aabc88@nate.com>
2025-08-06 14:54:22 +0900

    feat: 임시 Search 데이터

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/search/Search.kt
@@ -0,0 +1,9 @@
+package com.hihihihi.gureumpage.ui.search
+
+data class Search(
+    val imgUrl: String,
+    val title: String,
+    val author: String,
+    val publisher: String,
+    val page: Int
+)
