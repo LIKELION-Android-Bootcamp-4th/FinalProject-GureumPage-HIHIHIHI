80c6f749f4ba271c99d932c2844c89eb6a2984f6
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-07 22:38:07 +0900

    feat: 구름 앱바 구현
    - 바텀 내비 리플효과 제거 실패.. 대신 코드 정렬

---
+++ presentation/src/main/java/com/hihihihi/gureumpage/designsystem/components/GureumAppBar.kt
@@ -0,0 +1,44 @@
+package com.hihihihi.gureumpage.designsystem.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.hihihihi.gureumpage.R
+import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GureumAppBar(
+    navController: NavHostController,
+    title: String = "",
+    showUpButton: Boolean = true,
+    actions: @Composable () -> Unit = {},
+) {
+    CenterAlignedTopAppBar(
+        title = { TitleText(text = title) },
+        navigationIcon = {
+            if (showUpButton) {
+                IconButton(
+                    onClick = {
+                        navController.popBackStack()
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_arrow_left),
+                        contentDescription = "upButton",
+                        tint = GureumTheme.colors.gray900,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+            }
+        },
+        actions = { actions },
+    )
+}
