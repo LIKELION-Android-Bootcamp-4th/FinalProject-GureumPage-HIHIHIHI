5237db4b482041560f04a3f82bbe5f3fdc0e09de
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-07 14:59:58 +0900

    feat: 스위치 viewModel 연동

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/mypage/component/MyPageMenuSection.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/mypage/component/MyPageMenuSection.kt
@@ -3,13 +3,19 @@ package com.hihihihi.gureumpage.ui.mypage.component
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
 import com.hihihihi.gureumpage.designsystem.theme.GureumTypography
+import com.hihihihi.gureumpage.ui.mypage.MypageViewModel
 
 @Composable
-fun MyPageMenuSection() {
+fun MyPageMenuSection(viewModel: MypageViewModel = hiltViewModel()) {
+    val isDarkTheme by viewModel.isDarkTheme.collectAsState() // 다크 모드 상태 관찰
+
     val colors = GureumTheme.colors
     val typography = GureumTypography
 
@@ -23,7 +29,12 @@ fun MyPageMenuSection() {
         MyPageMenuSettingItem("오픈소스 라이선스") {
             //TODO: 기능 추가
         }
-        MyPageMenuSettingItem("다크모드", showSwitch = true) {
+        MyPageMenuSettingItem(
+            title = "다크모드",
+            showSwitch = true,
+            switchChecked = isDarkTheme,
+            onSwitchToggle = { viewModel.toggleTheme(it)}
+        ) {
             //TODO: 기능 추가
         }
         MyPageMenuSettingItem("로그아웃") {
