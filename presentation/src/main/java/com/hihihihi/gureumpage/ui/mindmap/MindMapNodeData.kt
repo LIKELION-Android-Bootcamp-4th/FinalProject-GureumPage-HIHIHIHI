e845d6821c0790aac3d1d14ac3d3d37ca556138a
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-07 17:14:58 +0900

    refactor: 노드에 아이콘 타입 변경 및 컬러 데이터 추가

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/mindmap/MindMapNodeData.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/mindmap/MindMapNodeData.kt
@@ -5,5 +5,6 @@ data class MindMapNodeData(
     val title: String,
     val content: String? = null,
     val bookImage: String? = null,  // 루트 노드 용 책 이미지
-    val icon: Int? = null,          // 아이콘 처리 방법 생각 필요 // TODO: 추후 enum 변경 고려
+    val icon: String? = null,
+    val color: Long? = null
 )
