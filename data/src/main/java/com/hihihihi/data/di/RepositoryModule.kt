5e5780c3bb2e9d198113662d75296e9ca533504b
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-08 11:31:46 +0900

    feat: Repository 의존성 주입 모듈 구성

--- data/src/main/java/com/hihihihi/data/di/RepositoryModule.kt
+++ data/src/main/java/com/hihihihi/data/di/RepositoryModule.kt
@@ -3,16 +3,19 @@ package com.hihihihi.data.di
 import com.hihihihi.data.remote.datasource.AuthDataSource
 import com.hihihihi.data.remote.datasource.KakaoDataSource
 import com.hihihihi.data.remote.datasource.NaverDataSource
+import com.hihihihi.data.remote.datasource.HistoryRemoteDataSource
 import com.hihihihi.data.remote.datasource.QuoteRemoteDataSource
 import com.hihihihi.data.remote.datasource.UserBookRemoteDataSource
 import com.hihihihi.data.repotisoryimpl.AuthRepositoryImpl
 import com.hihihihi.data.repotisoryimpl.KakaoAuthRepositoryImpl
 import com.hihihihi.data.repotisoryimpl.NaverAuthRepositoryImpl
+import com.hihihihi.data.repotisoryimpl.HistoryRepositoryImpl
 import com.hihihihi.data.repotisoryimpl.QuoteRepositoryImpl
 import com.hihihihi.data.repotisoryimpl.UserBookRepositoryImpl
 import com.hihihihi.domain.repository.AuthRepository
 import com.hihihihi.domain.repository.KakaoAuthRepository
 import com.hihihihi.domain.repository.NaverAuthRepository
+import com.hihihihi.domain.repository.HistoryRepository
 import com.hihihihi.domain.repository.QuoteRepository
 import com.hihihihi.domain.repository.UserBookRepository
 import dagger.Module
@@ -64,5 +67,13 @@ object RepositoryModule {
         kakaoDataSource: KakaoDataSource
     ): KakaoAuthRepository {
         return KakaoAuthRepositoryImpl(kakaoDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHistoryRepository(
+        remoteDataSource: HistoryRemoteDataSource
+    ) : HistoryRepository {
+        return HistoryRepositoryImpl(remoteDataSource)
     }
 }
