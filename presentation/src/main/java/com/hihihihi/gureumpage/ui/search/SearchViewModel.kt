b561ed8ee82744b6b0176a8a8ac680ec78117d97
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-10 23:45:58 +0900

    feat: addUserBook 적용

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/search/SearchViewModel.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/search/SearchViewModel.kt
@@ -2,19 +2,25 @@ package com.hihihihi.gureumpage.ui.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.hihihihi.domain.model.ReadingStatus
 import com.hihihihi.domain.model.SearchBook
+import com.hihihihi.domain.model.UserBook
 import com.hihihihi.domain.repository.SearchRepository
 import com.hihihihi.domain.usecase.search.SearchBooksUseCase
+import com.hihihihi.domain.usecase.userbook.AddUserBookUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     private val searchBooksUseCase: SearchBooksUseCase,
+    private val addUserBookUseCase: AddUserBookUseCase,
     private val searchRepository: SearchRepository
 ) : ViewModel() {
     private val _searchResults = MutableStateFlow<List<SearchBook>>(emptyList())
@@ -30,7 +36,8 @@ class SearchViewModel @Inject constructor(
             }
         }
     }
-    
+
+    // TODO: usecase로 일관성 맞추기
     fun getBookPageCount(isbn: String, onResult: (Int?) -> Unit) {
         viewModelScope.launch {
             try {
@@ -38,6 +45,46 @@ class SearchViewModel @Inject constructor(
                 onResult(pageCount)
             } catch (e: Exception) {
                 onResult(null)
+            }
+        }
+    }
+
+    fun addUserBook(
+        searchBook: SearchBook,
+        startDate: LocalDateTime,
+        endDate: LocalDateTime,
+        currentPage: Int,
+        totalPage: Int,
+        status: ReadingStatus
+    ) {
+        viewModelScope.launch {
+            try {
+                val userBook = UserBook(
+                    userBookId = "",
+                    userId = FirebaseAuth.getInstance().currentUser?.uid!!,
+                    isbn10 = "",
+                    isbn13 = searchBook.isbn,
+                    title = searchBook.title,
+                    author = searchBook.author,
+                    imageUrl = searchBook.coverImageUrl,
+                    isLiked = false,
+                    totalPage = totalPage,
+                    currentPage = currentPage,
+                    startDate = startDate,
+                    endDate = endDate,
+                    totalReadTime = 0,
+                    status = status,
+                    review = null,
+                    rating = null,
+                    category = searchBook.categoryName,
+                )
+
+                val result = addUserBookUseCase(userBook)
+
+                // TODO: 성공 실패 처리 하기
+                result.isSuccess
+            } catch (e: Exception) {
+
             }
         }
     }
