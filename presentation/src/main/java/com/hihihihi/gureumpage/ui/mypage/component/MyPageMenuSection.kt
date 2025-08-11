705751950ced93c98199aae27c129c97531fbc3e
KIMHAKROK <gkrfhr0039@naver.com>
2025-08-11 14:54:18 +0900

    refactor: 문의하기 코드 리팩토링

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/mypage/component/MyPageMenuSection.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/mypage/component/MyPageMenuSection.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hihihihi.gureumpage.common.utils.openAppOnPlayStore
+import com.hihihihi.gureumpage.common.utils.openSupportEmail
 import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
 import com.hihihihi.gureumpage.designsystem.theme.GureumTypography
 import com.hihihihi.gureumpage.ui.mypage.MypageViewModel
@@ -26,7 +27,7 @@ fun MyPageMenuSection(viewModel: MypageViewModel = hiltViewModel()) {
             openAppOnPlayStore(context)
         }
         MyPageMenuSettingItem("문의하기") {
-            //TODO: 기능 추가
+            openSupportEmail(context)
         }
         MyPageMenuSettingItem("오픈소스 라이선스") {
             //TODO: 기능 추가
