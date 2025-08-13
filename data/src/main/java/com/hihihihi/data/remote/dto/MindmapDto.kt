d6d9bba7d13ae376f7ca7096ad8c0c6cd557b30e
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-13 20:44:23 +0900

    fix: 파이어베이스와 코드에 다른 프로퍼티 네임 키로 찾아 매핑함으로 인해 못찾던 오류 수정

--- data/src/main/java/com/hihihihi/data/remote/dto/MindmapDto.kt
+++ data/src/main/java/com/hihihihi/data/remote/dto/MindmapDto.kt
@@ -1,15 +1,12 @@
 package com.hihihihi.data.remote.dto
 
 import androidx.annotation.Keep
-import com.google.firebase.firestore.PropertyName
 
 @Keep
 data class MindmapDto(
     var mindmapId: String = "",
 
-    @get:PropertyName("userbook_id") @set:PropertyName("userbook_id")
     var userBookId: String = "",
 
-    @get:PropertyName("root_node_id") @set:PropertyName("root_node_id")
     var rootNodeId: String? = null,
 )
--- data/src/main/java/com/hihihihi/data/remote/dto/MindmapNodeDto.kt
+++ data/src/main/java/com/hihihihi/data/remote/dto/MindmapNodeDto.kt
@@ -1,34 +1,24 @@
 package com.hihihihi.data.remote.dto
 
 import androidx.annotation.Keep
-import com.google.firebase.firestore.PropertyName
-import com.hihihihi.domain.model.MindmapNode
-import com.hihihihi.domain.model.MindmapNodeType
-
 
 @Keep
 data class MindmapNodeDto(
     var mindmapNodeId: String = "",
 
-    @get:PropertyName("mindmap_id") @set:PropertyName("mindmap_id")
     var mindmapId: String = "",
 
-    @get:PropertyName("node_title") @set:PropertyName("node_title")
     var nodeTitle: String = "",
 
-    @get:PropertyName("node_ex") @set:PropertyName("node_ex")
     var nodeEx: String = "",
 
-    @get:PropertyName("parent_node_id") @set:PropertyName("parent_node_id")
     var parentNodeId: String? = null,
 
-    @get:PropertyName("color") @set:PropertyName("color")
     var color: String? = null,
 
-    @get:PropertyName("icon") @set:PropertyName("icon")
     var icon: String? = null,
 
-    @get:PropertyName("deleted") @set:PropertyName("deleted")
     var deleted: Boolean = false,
 
+    var bookImage: String? = null,
 )
