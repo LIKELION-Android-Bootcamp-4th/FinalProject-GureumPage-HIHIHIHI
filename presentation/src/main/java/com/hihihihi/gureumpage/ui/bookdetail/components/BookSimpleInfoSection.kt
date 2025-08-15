cec5b02d06d3ac672baa36644c2f096939825bde
seeho <siki7878@gmail.com>
2025-08-16 02:22:25 +0900

    refactor: 테스트용 Text  제거

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/bookdetail/components/BookSimpleInfoSection.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/bookdetail/components/BookSimpleInfoSection.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,14 +13,11 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,12 +43,6 @@ fun BookSimpleInfoSection(
         ReadingStatus.READING -> GureumTheme.colors.systemGreen
         ReadingStatus.FINISHED -> GureumTheme.colors.primaryDeep
     }
-
-    Text(
-        text = "Status",
-        color = statusColor
-    )
-
 
     Row(
         modifier = Modifier
