0db8f1c8422858bb4f57813a5bd2c3bbc17a0558
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-17 18:44:03 +0900

    refactor: 바텀시트 독서 상태 아이템 변경

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/search/component/CategoryRow.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/search/component/CategoryRow.kt
@@ -14,27 +14,25 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.hihihihi.domain.model.ReadingStatus
 import com.hihihihi.gureumpage.R
+import com.hihihihi.gureumpage.designsystem.components.Medi12Text
+import com.hihihihi.gureumpage.designsystem.components.Semi14Text
 import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
 
 @Composable
 fun CategoryRow(
     title: String, subtitle: String, isSelected: Boolean, onClick: () -> Unit
 ) {
-    val backgroundColor =
-        if (isSelected) GureumTheme.colors.primary50 else GureumTheme.colors.background10
-    val borderColor =
-        if (isSelected) GureumTheme.colors.primary50 else GureumTheme.colors.background50
+    val backgroundColor = if (isSelected) GureumTheme.colors.primary50 else GureumTheme.colors.background10
+    val borderColor = if (isSelected) GureumTheme.colors.primary50 else GureumTheme.colors.background50
 
     Row(
         modifier = Modifier
@@ -52,15 +50,14 @@ fun CategoryRow(
                 .clip(CircleShape)
                 .background(if (isSelected) GureumTheme.colors.primary else Color.Transparent)
                 .border(
-                    1.5.dp,
-                    if (isSelected) Color.Transparent else GureumTheme.colors.gray500,
+                    width = 1.5.dp,
+                    color = if (isSelected) Color.Transparent else GureumTheme.colors.gray500,
                     shape = CircleShape
                 )
         ) {
             if (isSelected) {
                 Icon(
-                    //TODO 이미지 리소스 추가 필요
-                    painter = painterResource(R.drawable.ic_plus),
+                    painter = painterResource(R.drawable.ic_checked),
                     contentDescription = "선택 됨",
                     tint = GureumTheme.colors.white,
                     modifier = Modifier
@@ -72,20 +69,23 @@ fun CategoryRow(
         Spacer(modifier = Modifier.width(16.dp))
         // 텍스트
         Column(modifier = Modifier.weight(1f)) {
-            Text(
+            Semi14Text(
                 text = title,
-                fontWeight = FontWeight.Bold,
                 color = if (isSelected) GureumTheme.colors.gray700 else GureumTheme.colors.gray500,
-                fontSize = 14.sp
             )
-            Text(text = subtitle, color = GureumTheme.colors.gray500, fontSize = 12.sp)
+            Medi12Text(text = subtitle, color = GureumTheme.colors.gray500)
         }
         Spacer(modifier = Modifier.width(16.dp))
 
         // 아이콘
-        //TODO 다른 아이콘 리소스 추가 필요
         Icon(
-            painter = painterResource(id = if(title == "읽는 중") R.drawable.ic_cloud_reading else R.drawable.ic_launcher_background),
+            painter = painterResource(
+                id = when (title) {
+                    ReadingStatus.PLANNED.displayName -> R.drawable.ic_cloud_planned
+                    ReadingStatus.READING.displayName -> R.drawable.ic_cloud_reading
+                    else -> R.drawable.ic_cloud_finished
+                }
+            ),
             contentDescription = title,
             modifier = Modifier.size(40.dp),
             tint = Color.Unspecified
