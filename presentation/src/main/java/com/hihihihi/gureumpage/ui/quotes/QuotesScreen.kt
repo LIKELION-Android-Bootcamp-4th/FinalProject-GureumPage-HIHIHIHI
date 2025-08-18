15986965154a77a05d4dfa646e40f76439510759
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-19 01:29:41 +0900

    feat: 필사가 없을 때 나타낼 문구 구현

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/QuotesScreen.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/quotes/QuotesScreen.kt
@@ -1,7 +1,9 @@
 package com.hihihihi.gureumpage.ui.quotes
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -15,8 +17,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hihihihi.domain.model.Quote
+import com.hihihihi.gureumpage.designsystem.components.Medi16Text
+import com.hihihihi.gureumpage.designsystem.components.Semi18Text
 import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
 import com.hihihihi.gureumpage.ui.quotes.component.QuoteContent
 
@@ -41,6 +46,14 @@ fun QuotesScreen(
         uiState.errorMessage != null -> {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 Text(text = "에러 발생", color = GureumTheme.colors.systemRed)
+            }
+        }
+
+        !uiState.isLoading && uiState.quotes.isEmpty() -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Semi18Text("아직 필사가 없어요")
+                Spacer(Modifier.height(16.dp))
+                Medi16Text("책에서 인상 깊은 한 줄을 남겨 보세요.")
             }
         }
 
