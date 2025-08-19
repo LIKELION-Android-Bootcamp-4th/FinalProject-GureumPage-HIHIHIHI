3e87d3f06c27fdafe504ea4c33391818859cf6c0
seeho <siki7878@gmail.com>
2025-08-19 09:48:11 +0900

    fix: 회원가입 후 홈으로 오면 유저가 null error 가 뜨던 문제 해결

--- data/src/main/java/com/hihihihi/data/remote/datasourceimpl/UserRemoteDataSourceImpl.kt
+++ data/src/main/java/com/hihihihi/data/remote/datasourceimpl/UserRemoteDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.hihihihi.data.remote.datasourceimpl
 
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.hihihihi.data.mapper.toDomain
 import com.hihihihi.data.remote.datasource.UserRemoteDataSource
@@ -25,23 +26,28 @@ class UserRemoteDataSourceImpl @Inject constructor(
     }
 
     override fun getUserFlow(userId: String): Flow<UserDto> = callbackFlow {
+        Log.e("TAG", "getUserFlow: ${userId}")
         val docRef = firestore.collection("users").document(userId)
 
         val listenerRegistration = docRef.addSnapshotListener { snapshot, error ->
             if (error != null) {
-                close(error) 
+                close(error)
                 return@addSnapshotListener
             }
 
-            val dto = snapshot?.toObject(UserDto::class.java)
-            if (dto != null) {
-                dto.userId = snapshot.id
-                trySend(dto).isSuccess
+
+            if (snapshot?.exists() == true) {
+                val dto = snapshot?.toObject(UserDto::class.java)
+                if (dto != null) {
+                    dto.userId = snapshot.id
+                    trySend(dto).isSuccess
+                } else {
+                    close(Exception("User 파싱 실패"))
+                }
             } else {
-                close(Exception("User 조회 실패"))
+                Log.e("TAG", "getUserFlow: 유저 없음")
             }
         }
-
         awaitClose { listenerRegistration.remove() }
     }
 
