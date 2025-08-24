b3b66cdebb8fe32cc0df99842396586533e83c09
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-24 23:17:13 +0900

    refactor: 100퍼 알림 미사용 처리

--- presentation/src/main/java/com/hihihihi/gureumpage/notification/progress/DailyGoalNotifier.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/notification/progress/DailyGoalNotifier.kt
@@ -39,37 +39,35 @@ object DailyGoalNotifier {
                 putString("day", today)
                     .putFloat("last_ratio", 0f)
                     .putBoolean("sent80", false)
-                    .putBoolean("sent100", false)
+//                    .putBoolean("sent100", false)
             }
         }
 
         val prev = sharedPref.getFloat("last_ratio", 0f)
         var sent80 = sharedPref.getBoolean("sent80", false)
-        var sent100 = sharedPref.getBoolean("sent100", false)
+//        var sent100 = sharedPref.getBoolean("sent100", false)
 
         val factory = NotificationFactory(context)
 
-        // 80%
-        // 80% 알림을 보내지 않고, 이전 값이 80보다 작으며 현재 값이 80보다 크거나 같을 때
+        // 80% - 알림을 보내지 않고, 이전 값이 80보다 작으며 현재 값이 80보다 크거나 같을 때
         if (!sent80 && prev < 0.8f && ratio >= 0.8f) {
             val pendingIntent = factory.pendingIntentTo("gureum://goals/today".toUri())
             val notification = factory.simpleAlarm(Channels.PROGRESS, "오늘 목표 80% 달성", "", pendingIntent)
-            factory.notify("goals:today", 12080, notification)
+            factory.notify("goals:today", 12000, notification)
             sent80 = true
         }
 
         // 100%
-        if (!sent100 && prev < 1.0f && ratio >= 1.0f) {
-            val pendingIntent = factory.pendingIntentTo("gureum://stats/daily".toUri())
-            val notification = factory.simpleAlarm(Channels.PROGRESS, "오늘 목표 달성 🎉 통계 보기", "", pendingIntent)
-            factory.notify("goals:today", 12100, notification)
-            sent100 = true
-        }
+//        if (!sent100 && prev < 1.0f && ratio >= 1.0f) {
+//            val pendingIntent = factory.pendingIntentTo("gureum://stats/daily".toUri())
+//            val notification = factory.simpleAlarm(Channels.PROGRESS, "오늘 목표 달성 🎉 통계 보기", "", pendingIntent)
+//            factory.notify("goals:today", 12100, notification)
+//            sent100 = true
+//        }
 
         sharedPref.edit {
-            putFloat("last_ratio", ratio)
-                .putBoolean("sent80", sent80)
-                .putBoolean("sent100", sent100)
+            putFloat("last_ratio", ratio).putBoolean("sent80", sent80)
+//                .putBoolean("sent100", sent100)
         }
     }
 }
