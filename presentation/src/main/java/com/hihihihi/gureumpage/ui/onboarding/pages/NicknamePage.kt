805427eca712f33ca6d55aff03670a908825a0fe
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-08-27 00:56:14 +0900

    refactor: 온보딩 닉네임 검증 기능 변경

--- presentation/src/main/java/com/hihihihi/gureumpage/ui/onboarding/pages/NicknamePage.kt
+++ presentation/src/main/java/com/hihihihi/gureumpage/ui/onboarding/pages/NicknamePage.kt
@@ -17,7 +17,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hihihihi.gureumpage.R
-import com.hihihihi.gureumpage.common.utils.validateNickname
+import com.hihihihi.gureumpage.common.utils.NicknameRule
+import com.hihihihi.gureumpage.common.utils.NicknameValidator
 import com.hihihihi.gureumpage.designsystem.components.GureumTextField
 import com.hihihihi.gureumpage.designsystem.theme.GureumPageTheme
 import com.hihihihi.gureumpage.designsystem.theme.GureumTheme
@@ -45,15 +46,16 @@ fun NicknamePage(viewModel: OnBoardingViewModel) {
             modifier = Modifier.padding(horizontal = 24.dp),
             horizontalAlignment = Alignment.End
         ) {
+            val rule = NicknameValidator.validate(viewModel.nickname)
             GureumTextField(
                 value = viewModel.nickname,
                 onValueChange = {
-                    viewModel.updateNickname(it.trim())
+                    viewModel.updateNickname(it)
                 },
                 hint = "닉네임을 입력해주세요",
                 textAlign = TextAlign.Center,
                 imeAction = ImeAction.Done,
-                isError = !viewModel.nickname.validateNickname(),
+                isError = rule !is NicknameRule.Ok,
             ) {
                 Text(
                     text = buildAnnotatedString {
