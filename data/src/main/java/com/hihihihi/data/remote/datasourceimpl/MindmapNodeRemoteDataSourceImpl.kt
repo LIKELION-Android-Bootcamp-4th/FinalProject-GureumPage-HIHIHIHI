c0036cef6e133c924b8175ce4e6d08936152c6e1
seeho <siki7878@gmail.com>
2025-08-27 00:30:20 +0900

    refactor: 마인드맵 노드 추가 시 user_id 들어가도록 함 but 임시방편임

--- data/src/main/java/com/hihihihi/data/remote/datasourceimpl/MindmapNodeRemoteDataSourceImpl.kt
+++ data/src/main/java/com/hihihihi/data/remote/datasourceimpl/MindmapNodeRemoteDataSourceImpl.kt
@@ -1,5 +1,7 @@
 package com.hihihihi.data.remote.datasourceimpl
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.hihihihi.domain.operation.NodeEditOperation
@@ -45,6 +47,9 @@ class MindmapNodeRemoteDataSourceImpl @Inject constructor(
         mindmapId: String,
         operations: List<NodeEditOperation>
     ): Result<Unit> = runCatching {
+        val currentUserId = FirebaseAuth.getInstance().currentUser?.uid
+            ?: throw IllegalStateException("사용자가 로그인되지 않았습니다")
+
         // 파이어스토어 배치가 500 쓰기 제한있음
         operations.chunked(450).forEach { chunk ->
             firebase.runBatch { batch ->
@@ -55,7 +60,7 @@ class MindmapNodeRemoteDataSourceImpl @Inject constructor(
                             else nodesCollection().document(operation.node.mindmapNodeId)
 
                             val dto = operation.node.copy(
-                                userId = operation.node.userId,
+                                userId = currentUserId,
                                 mindmapId = mindmapId,
                                 mindmapNodeId = document.id,
                                 deleted = false
@@ -81,7 +86,10 @@ class MindmapNodeRemoteDataSourceImpl @Inject constructor(
                             requireNotNull(operation.node.mindmapNodeId.takeIf { it.isNotBlank() }) { "노드 아이디가 필요합니다" }
                             batch.set(
                                 nodesCollection().document(operation.node.mindmapNodeId),
-                                operation.node.copy(mindmapId = mindmapId),
+                                operation.node.copy(
+                                    mindmapId = mindmapId,
+                                    userId = currentUserId
+                                ),
                                 SetOptions.merge()
                             )
                         }
