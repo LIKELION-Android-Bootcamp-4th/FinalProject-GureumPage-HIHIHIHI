6d896074f3e1746ec7a10a2d74dda9b59cc4386d
seeho <siki7878@gmail.com>
2025-08-28 14:43:44 +0900

    chore: versionCode 5->6

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.hihihihi.gureumpage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
