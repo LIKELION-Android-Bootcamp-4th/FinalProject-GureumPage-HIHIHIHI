f8e965af239c1313a4d452beeca4f0befcb1d504
seeho <siki7878@gmail.com>
2025-08-28 22:19:18 +0900

    chore: versionCode 6->7, versionName 1.1.0 -> 1.1.1

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.hihihihi.gureumpage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
