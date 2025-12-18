4a804fe3f4b7414802ea1702faf470c52930b3c2
seeho <siki7878@gmail.com>
2025-12-18 19:18:45 +0900

    chore: update versionCode

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.hihihihi.gureumpage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
+        versionCode = 9
         versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
