bfcfafc5485d3f24fa70b634473cd2a39c3ffff2
seeho <siki7878@gmail.com>
2026-01-16 18:36:47 +0900

    chore: versionName 1.1.3, versionCode 10 으로 변경

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.hihihihi.gureumpage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.2"
+        versionCode = 10
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
