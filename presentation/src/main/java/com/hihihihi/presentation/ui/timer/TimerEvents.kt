dd9cc8e734a8184662a2168ae884d330b546b1c9
김의현 <hyunkim6790@gmail.com>
2026-04-17 19:40:46 +0900

    refactor: TimerDialogState 및 TimerEffect를 sealed interface로 변경
    - TimerDialogState와 TimerEffect를 `sealed class`에서 `sealed interface`로 변경
    - 하위 `object`들을 `data object`로 변경하여 최신 Kotlin 스타일 적용 및 최적화

--- presentation/src/main/java/com/hihihihi/presentation/ui/timer/TimerEvents.kt
+++ presentation/src/main/java/com/hihihihi/presentation/ui/timer/TimerEvents.kt
@@ -1,15 +1,15 @@
 package com.hihihihi.presentation.ui.timer
 
-sealed class TimerDialogState {
-    object None : TimerDialogState()
-    object Memo : TimerDialogState()
-    data class StopConfirm(val wasRunning: Boolean) : TimerDialogState()
-    data class BackExit(val wasRunning: Boolean) : TimerDialogState()
+sealed interface TimerDialogState {
+    data object None : TimerDialogState
+    data object Memo : TimerDialogState
+    data class StopConfirm(val wasRunning: Boolean) : TimerDialogState
+    data class BackExit(val wasRunning: Boolean) : TimerDialogState
 }
 
 enum class TimerDialogType { Memo, StopConfirm, BackExit }
 
-sealed class TimerEffect {
-    object RequestOverlayPermission : TimerEffect()
-    object StartFloatingWindow : TimerEffect()
+sealed interface TimerEffect {
+    data object RequestOverlayPermission : TimerEffect
+    data object StartFloatingWindow : TimerEffect
 }
